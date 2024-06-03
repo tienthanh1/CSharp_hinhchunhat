0a01f5c6adad7c41948e674986540abf056c98a2
tien thanh <tienthanh@gmail.com>
2024-06-03 15:47:07 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CSharp_hinhchunhat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbla = new System.Windows.Forms.Label();
+            this.txtso1 = new System.Windows.Forms.TextBox();
+            this.lblb = new System.Windows.Forms.Label();
+            this.txtso2 = new System.Windows.Forms.TextBox();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.lblkq = new System.Windows.Forms.Label();
+            this.btnchuvi = new System.Windows.Forms.Button();
+            this.btndientich = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lbla
+            // 
+            this.lbla.AutoSize = true;
+            this.lbla.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbla.Location = new System.Drawing.Point(152, 54);
+            this.lbla.Name = "lbla";
+            this.lbla.Size = new System.Drawing.Size(20, 23);
+            this.lbla.TabIndex = 0;
+            this.lbla.Text = "a";
+            // 
+            // txtso1
+            // 
+            this.txtso1.Location = new System.Drawing.Point(358, 54);
+            this.txtso1.Name = "txtso1";
+            this.txtso1.Size = new System.Drawing.Size(116, 20);
+            this.txtso1.TabIndex = 1;
+            // 
+            // lblb
+            // 
+            this.lblb.AutoSize = true;
+            this.lblb.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblb.Location = new System.Drawing.Point(151, 109);
+            this.lblb.Name = "lblb";
+            this.lblb.Size = new System.Drawing.Size(21, 23);
+            this.lblb.TabIndex = 2;
+            this.lblb.Text = "b";
+            // 
+            // txtso2
+            // 
+            this.txtso2.Location = new System.Drawing.Point(358, 109);
+            this.txtso2.Name = "txtso2";
+            this.txtso2.Size = new System.Drawing.Size(116, 20);
+            this.txtso2.TabIndex = 3;
+            this.txtso2.TextChanged += new System.EventHandler(this.txtso2_TextChanged);
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(358, 166);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(116, 20);
+            this.txtkq.TabIndex = 5;
+            this.txtkq.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // lblkq
+            // 
+            this.lblkq.AutoSize = true;
+            this.lblkq.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblkq.Location = new System.Drawing.Point(152, 166);
+            this.lblkq.Name = "lblkq";
+            this.lblkq.Size = new System.Drawing.Size(75, 23);
+            this.lblkq.TabIndex = 4;
+            this.lblkq.Text = "kết quả ";
+            this.lblkq.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnchuvi
+            // 
+            this.btnchuvi.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnchuvi.Location = new System.Drawing.Point(53, 273);
+            this.btnchuvi.Name = "btnchuvi";
+            this.btnchuvi.Size = new System.Drawing.Size(92, 33);
+            this.btnchuvi.TabIndex = 6;
+            this.btnchuvi.Text = "chu Vi";
+            this.btnchuvi.UseVisualStyleBackColor = true;
+            this.btnchuvi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btndientich
+            // 
+            this.btndientich.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndientich.Location = new System.Drawing.Point(424, 273);
+            this.btndientich.Name = "btndientich";
+            this.btndientich.Size = new System.Drawing.Size(98, 33);
+            this.btndientich.TabIndex = 7;
+            this.btndientich.Text = "Diện tích";
+            this.btndientich.UseVisualStyleBackColor = true;
+            this.btndientich.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(244, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Tính HCN";
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthoat.Location = new System.Drawing.Point(248, 354);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(92, 33);
+            this.btnthoat.TabIndex = 10;
+            this.btnthoat.Text = "thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 450);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btndientich);
+            this.Controls.Add(this.btnchuvi);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.lblkq);
+            this.Controls.Add(this.txtso2);
+            this.Controls.Add(this.lblb);
+            this.Controls.Add(this.txtso1);
+            this.Controls.Add(this.lbla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbla;
+        private System.Windows.Forms.TextBox txtso1;
+        private System.Windows.Forms.Label lblb;
+        private System.Windows.Forms.TextBox txtso2;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Label lblkq;
+        private System.Windows.Forms.Button btnchuvi;
+        private System.Windows.Forms.Button btndientich;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnthoat;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_hinhchunhat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Double so1, so2, ketqua;
+            so1 = Convert.ToDouble(txtso1.Text);
+            so2 = Convert.ToDouble(txtso2.Text);
+            ketqua = so1 * so2;
+            txtkq.Text=ketqua.ToString();
+            lblkq.Text=ketqua.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double so1, so2, ketqua;
+            so1 = Convert.ToDouble(txtso1.Text);
+            so2 = Convert.ToDouble(txtso2.Text);
+            ketqua = (so1 + so2)*2;
+            txtkq.Text = ketqua.ToString();
+            lblkq.Text = ketqua.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtso2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
